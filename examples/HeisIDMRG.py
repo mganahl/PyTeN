715fb4890d1a5f7cd43fc801a8c5b1c0702bffae
mganahl <mganahl@pitp.ca>
2018-07-02 16:18:51 -0400

    edited HeisIDMG example file

--- examples/HeisIDMRG.py
+++ examples/HeisIDMRG.py
@@ -1,7 +1,6 @@
 """
 @author: Martin Ganahl
 """
-
 import sys,os
 root=os.getcwd()
 os.chdir('../')
@@ -20,23 +19,22 @@ anticomm=lambda x,y:np.dot(x,y)+np.dot(y,x)
 herm=lambda x:np.conj(np.transpose(x))
 
 if __name__ == "__main__":
-    D=100
-    D0=10
+    D=10
     d=2
-    N=40
+    N=2
     Jz=np.ones(N)
     Jxy=np.ones(N)
-    #initialize MPS with bond dimension D0
-    mps=mpslib.MPS.random(N=N,D=D0,d=d,obc=False,dtype=float)
+    #initialize MPS with bond dimension D
+    mps=mpslib.MPS.random(N=N,D=D,d=d,obc=False,dtype=float)
 
-    #set the bond-dimension cutoff to D
-    #mps._D=D
+
     mps.__position__(N)
     mps.__position__(0)
     mpoobc=H.XXZ(Jz,Jxy,np.zeros(N),False)
-    lb=np.ones((D0,D0,1))
-    rb=np.ones((D0,D0,1))
+    lb=np.ones((D,D,1))
+    rb=np.ones((D,D,1))
     idmrg=en.IDMRGengine(mps,mpoobc,'blabla')
-    idmrg.__simulateTwoSite__(Nmax=100,NUC=2,Econv=1E-10,tol=1E-6,ncv=20,cp=None,verbose=1,truncation=1E-8)
-    #idmrg.__simulate__(Nmax=100,NUC=2,Econv=1E-10,tol=1E-6,ncv=20,cp=None,verbose=1)
+    
+    idmrg.__simulateTwoSite__(Nmax=100,NUC=2,Econv=1E-10,tol=1E-6,ncv=20,cp=None,verbose=1,truncation=1E-8) #two site idmrg
+    #idmrg.__simulate__(Nmax=100,NUC=2,Econv=1E-10,tol=1E-6,ncv=20,cp=None,verbose=1) #single site idmrg
 
