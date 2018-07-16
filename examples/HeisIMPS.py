some minor bug fixes in TFI.py added some additional parameter flags for the gradient optimization (pinv,truncation threshold, ..) added VUMPSengine for variatonal gs search for infinite systems

examples/HeisIMPS.py
@@ -32,6 +32,8 @@ if __name__ == "__main__":
     parser.add_argument('--normgrads', nargs='+',help='list of length N=len(dts) (see above); if norm(xdot)<nxdots[i], use dts[i] for imaginary time evolution',type=float)
     parser.add_argument('--lgmrestol', help='lgmres tolerance for reduced hamiltonians (1E-10)',type=float,default=1E-6)
     parser.add_argument('--regaugetol', help='tolerance of eigensolver for finding left and right reduced DM (1E-10)',type=float,default=1E-6)
+    parser.add_argument('--pinv', help='pseudo-inverse cutoof; use default value if no pseudo-inverse should be done (1E-100)',type=float,default=1E-100)
+    parser.add_argument('--truncation', help='truncation threshold; if schmidt-values < truncation are found, they are truncated; this changeds the bond dimension',type=float,default=1E-100)            
     parser.add_argument('--epsilon', help='desired convergence of the gradient (1E-5)',type=float,default=1E-6)
     parser.add_argument('--imax', help='maximum number of iterations (20000)',type=int,default=20000)
     parser.add_argument('--saveit', help='save the simulation every saveit iterations for checkpointing (10)',type=int,default=10)
@@ -66,7 +68,6 @@ if __name__ == "__main__":
     Jxy=np.ones(N)
     B=args.B*np.ones(N)
     mpo=H.XXZ(Jz,Jxy,B,False)[0]
-    print (mpo.shape)
 
     #mps=(np.random.rand(args.D,args.D,2)-0.5+1j*(np.random.rand(args.D,args.D,2)-0.5))*0.5
     if args.dtype=='complex':
@@ -83,7 +84,7 @@ if __name__ == "__main__":
     [mps,lam]=mf.regauge(tensor,gauge='left',tol=args.regaugetol)
     iMPS=en.HomogeneousIMPSengine(args.imax,mps,mpo,args.filename,args.alpha,args.alphas,args.normgrads,dtype,args.rescalingfactor,\
                                   args.nreset,args.normtolerance,args.epsilon,args.regaugetol,args.lgmrestol,args.ncv,args.numeig,\
-                                  args.Nmaxlgmres)
+                                  args.Nmaxlgmres,pinv=args.pinv,trunc=args.truncation)
 
     iMPS.__simulate__()
     np.save(args.filename,iMPS._mps)

examples/TFI.py
@@ -26,20 +26,23 @@ if __name__ == "__main__":
     parser.add_argument('--Jx', help='Jx intercation (1.0)',type=float,default=1.0)
     parser.add_argument('--Bz', help='magnetic field (0.5)',type=float,default=0.5)
     parser.add_argument('--rescalingfactor',help='hyperparamter: rescaling factor by which time step is rescaled if norm increase is detected (2.0)',type=float,default=2.0)
-    parser.add_argument('--normtolerance',help='hyperparamter: tolerance of relative normincrease (0.1)',type=float,default=0.1)
-    parser.add_argument('--alpha',help='gradient stepsize (0.05)',type=float,default=0.005)
+    parser.add_argument('--scaling',help='scaling of the initial MPS entries (0.5)',type=float,default=0.5)    
+    parser.add_argument('--normtolerance',help='hyperparamter: tolerance of relative normincrease (0.5)',type=float,default=0.5)
+    parser.add_argument('--alpha',help='gradient stepsize (0.001)',type=float,default=0.001)
     parser.add_argument('--alphas', nargs='+',help='list of time steps for imaginary time evolution',type=float)
     parser.add_argument('--normgrads', nargs='+',help='list of length N=len(dts) (see above); if norm(xdot)<nxdots[i], use dts[i] for imaginary time evolution',type=float)
-    parser.add_argument('--lgmrestol', help='lgmres tolerance for reduced hamiltonians (1E-10)',type=float,default=1E-6)
-    parser.add_argument('--regaugetol', help='tolerance of eigensolver for finding left and right reduced DM (1E-10)',type=float,default=1E-6)
-    parser.add_argument('--epsilon', help='desired convergence of the gradient (1E-5)',type=float,default=1E-6)
+    parser.add_argument('--lgmrestol', help='lgmres tolerance for reduced hamiltonians (1E-10)',type=float,default=1E-10)
+    parser.add_argument('--pinv', help='pseudo-inverse cutoof; use default value if no pseudo-inverse should be done (1E-100)',type=float,default=1E-100)
+    parser.add_argument('--truncation', help='truncation threshold; if schmidt-values < truncation are found, they are truncated; this changeds the bond dimension',type=float,default=1E-100)                
+    parser.add_argument('--regaugetol', help='tolerance of eigensolver for finding left and right reduced DM (1E-10)',type=float,default=1E-10)
+    parser.add_argument('--epsilon', help='desired convergence of the gradient (1E-6)',type=float,default=1E-6)
     parser.add_argument('--imax', help='maximum number of iterations (20000)',type=int,default=20000)
     parser.add_argument('--saveit', help='save the simulation every saveit iterations for checkpointing (10)',type=int,default=10)
     parser.add_argument('--nreset', help='number of steps at each level of the adaptive dt refinement (10)',type=int,default=10)
-    parser.add_argument('--filename', help='filename for output (_cMPSoptnew)',type=str,default='_HeisIMPS')
+    parser.add_argument('--filename', help='filename for output (_cMPSoptnew)',type=str,default='_TFI')
     parser.add_argument('--seed', help='seed for initialization of Q and R matrices',type=int)
-    parser.add_argument('--numeig', help='number of eigenvector in TMeigs',type=int,default=5)
-    parser.add_argument('--ncv', help='number of krylov vectors in TMeigs',type=int,default=20)
+    parser.add_argument('--numeig', help='number of eigenvector in TMeigs (5)',type=int,default=5)
+    parser.add_argument('--ncv', help='number of krylov vectors in TMeigs (20)',type=int,default=20)
     parser.add_argument('--Nmaxlgmres', help='Maximum number of lgmres steps (see scipy.sparse.linalg.lgmres help); total number is (Nmaxlgmres+1)*innermlgmres',type=int,default=50)
     parser.add_argument('--outerklgmres', help='Number of vectors to carry between inner GMRES iterations. According to [R271], good values are in the range of 1...3. However, note that if you want to use the additional vectors to accelerate solving multiple similar problems, larger values may be beneficial (from scipy.sparse.linalg.lgmres manual)',type=int,default=10)
     parser.add_argument('--innermlgmres', help='Number of inner GMRES iterations per each outer iteration (from scipy.sparse.linalg.lgmres manual)',type=int,default=30)
@@ -57,9 +60,6 @@ if __name__ == "__main__":
             print ('please enter same number of values for --alphas and --normgrads')
             sys.exit()
 
-
-
-    args=parser.parse_args()
     d=2
     N=1
     Jx=args.Jx*np.ones(N)
@@ -68,10 +68,10 @@ if __name__ == "__main__":
 
     #mps=(np.random.rand(args.D,args.D,2)-0.5+1j*(np.random.rand(args.D,args.D,2)-0.5))*0.5
     if args.dtype=='complex':
-        tensor=(np.random.rand(args.D,args.D,d)-0.5)*0.9+1j*(np.random.rand(args.D,args.D,d)-0.5)*0.9
+        tensor=(np.random.rand(args.D,args.D,d)-0.5)*args.scaling+1j*(np.random.rand(args.D,args.D,d)-0.5)*args.scaling
         dtype=complex
     elif args.dtype=='float':
-        tensor=(np.random.rand(args.D,args.D,d)-0.5)*0.9
+        tensor=(np.random.rand(args.D,args.D,d)-0.5)*args.scaling
         dtype=float        
     else:
         sys.exit('unknown type args.dtype={0}'.format(args.dtype))
@@ -79,9 +79,10 @@ if __name__ == "__main__":
         
     filename=args.filename+'D{0}_Jx{1}_B{2}'.format(args.D,args.Jx,args.Bz)
     [mps,lam]=mf.regauge(tensor,gauge='left',tol=args.regaugetol)
+    
     iMPS=en.HomogeneousIMPSengine(args.imax,mps,mpo,args.filename,args.alpha,args.alphas,args.normgrads,dtype,args.rescalingfactor,\
                                   args.nreset,args.normtolerance,args.epsilon,args.regaugetol,args.lgmrestol,args.ncv,args.numeig,\
-                                  args.Nmaxlgmres)
+                                  args.Nmaxlgmres,pinv=args.pinv,trunc=args.truncation)
 
     iMPS.__simulate__()
 

lib/mpslib/engines.py
@@ -484,7 +484,7 @@ class HomogeneousIMPSengine:
     Nmaxlgmres (int): max steps of the lgmres routine used to calculate the infinite environments
     """
 
-    def __init__(self,Nmax,mps,mpo,filename,alpha,alphas,normgrads,dtype,factor=2.0,itreset=10,normtol=0.1,epsilon=1E-10,tol=1E-10,lgmrestol=1E-10,ncv=30,numeig=3,Nmaxlgmres=40):
+    def __init__(self,Nmax,mps,mpo,filename,alpha,alphas,normgrads,dtype,factor=2.0,itreset=10,normtol=0.1,epsilon=1E-10,tol=1E-10,lgmrestol=1E-10,ncv=30,numeig=3,Nmaxlgmres=40,pinv=1E-100,trunc=1E-16):
         self._Nmax=Nmax
         self._mps=np.copy(mps)
         self._D=np.shape(mps)[0]
@@ -502,7 +502,8 @@ class HomogeneousIMPSengine:
         self._alphas=alphas
         self._alpha=alpha
         self._alpha_=alpha
-
+        self._pinv=pinv
+        self._trunc=trunc
         self._normgrads=normgrads
         self._normtol=normtol
         self._factor=factor
@@ -537,8 +538,7 @@ class HomogeneousIMPSengine:
     def __doGradStep__(self):
         converged=False
         self._gamma,self._lam,trunc=mf.regauge(self._mps,gauge='symmetric',initial=np.reshape(np.diag(self._lam**2),self._D*self._D),\
-                                               nmaxit=10000,tol=self._tol,ncv=self._ncv,numeig=self._numeig,trunc=1E-12,Dmax=self._D)
-        
+                                               nmaxit=10000,tol=self._tol,ncv=self._ncv,numeig=self._numeig,trunc=self._trunc,Dmax=self._D,pinv=self._pinv)
         if len(self._lam)!=self._D:
             Dchanged=True
             self._D=len(self._lam)
@@ -629,7 +629,7 @@ class HomogeneousIMPSengine:
             if self._it%checkpoint==0:
                 np.save('CPTensor'+self._filename,self._mps)
             self._it+=1
-            stdout.write("\rit %i: local E=%.16f, lnorm=%.6f, rnorm=%.6f, grad=%.16f, alpha=%.4f" %(self._it,np.real(Elocleft),leftn,rightn,self._normgrad,self._alpha_))
+            stdout.write("\rit %i: local E=%.16f, lnorm=%.6f, rnorm=%.6f, grad=%.16f, alpha=%.4f, D=%i" %(self._it,np.real(Elocleft),leftn,rightn,self._normgrad,self._alpha_,self._D))
             stdout.flush()
         print
         if self._it>=self._Nmax and (converged==False):
@@ -668,6 +668,160 @@ class HomogeneousDiscretizedBosonEngine(HomogeneousIMPSengine):
         print
         
 
+
+class VUMPSengine:
+    """
+
+    VUMPSengine.__init__(Nmax,mps,mpo,filename,alpha,alphas,normgrads,dtype,factor=2.0,itreset=10,normtol=0.1,epsilon=1E-10,tol=1E-4,lgmrestol=1E-10,ncv=30,numeig=3,Nmaxlgmres=40):
+    initialize a homogeneous VUMPS optimization
+    
+    MPS optimization methods for homogeneous systems
+    uses gradient optimization to find the ground state of a Homogeneous system
+    mps (np.ndarray): an initial mps tensor 
+    mpo (np.ndarray): the mpo tensor
+    filename (str): filename of the simulation
+    alpha (float): initial steps size
+    alphas (list of float): alphas[i] is the stepsizes to be use once gradient norm is smaller than normgrads[i] (see next)
+    normgrads (list of float): alphas[i] is the stepsizes to be use once gradient norm is smaller than normgrads[i] (see next)
+    dtype: type of the mps (float or complex)
+    factor (float): factor by which internal stepsize is reduced in case divergence is detected
+    normtol (float): absolute value by which gradient may increase without raising a "divergence" flag
+    epsilon (float): desired convergence of the gradient
+    tol (float): eigensolver tolerance used in regauging
+    lgmrestol (float): eigensolver tolerance used for calculating the infinite environements
+    ncv (int): number of krylov vectors used in sparse eigensolvers
+    numeig (int): number of eigenvectors to be calculated in the sparse eigensolver
+    Nmaxlgmres (int): max steps of the lgmres routine used to calculate the infinite environments
+    """
+
+    def __init__(self,Nmax,mps,mpo,filename,dtype,epsilon=1E-10,tol=1E-10,lgmrestol=1E-10,ncv=30,numeig=3,Nmaxlgmres=40,\
+                 artol=1E-6,arnumvecs=1,arncv=20,svd=False):
+
+        self._svd=svd
+        self._artol=artol
+        self._arnumvecs=arnumvecs
+        self._arncv=arncv
+        self._Nmax=Nmax
+        self._mps=np.copy(mps)
+        self._D=np.shape(mps)[0]
+        self._filename=filename
+        self._dtype=dtype
+        self._tol=tol
+        self._lgmrestol=lgmrestol
+        self._numeig=numeig
+        self._ncv=ncv
+        self._kleft=np.random.rand(self._D,self._D)
+        self._kright=np.random.rand(self._D,self._D)
+        self._epsilon=epsilon
+        self._Nmaxlgmres=Nmaxlgmres
+        self._it=1
+        [B1,B2,d1,d2]=np.shape(mpo)
+
+        mpol=np.zeros((1,B2,d1,d2),dtype=self._dtype)
+        mpor=np.zeros((B1,1,d1,d2),dtype=self._dtype)
+
+        mpol[0,:,:,:]=mpo[-1,:,:,:]
+        mpol[0,0,:,:]/=2.0
+        mpor[:,0,:,:]=mpo[:,0,:,:]
+        mpor[-1,0,:,:]/=2.0
+
+        self._mpo=[]
+        self._mpo.append(np.copy(mpol))
+        self._mpo.append(np.copy(mpo))
+        self._mpo.append(np.copy(mpor))
+
+
+
+    def __doStep__(self):
+        [etar,vr,numeig]=mf.TMeigs(self._A,direction=-1,numeig=self._numeig,init=self._r,nmax=10000,tolerance=self._tol,ncv=self._ncv,which='LR')
+        [etal,vl,numeig]=mf.TMeigs(self._B,direction=1,numeig=self._numeig,init=self._r,nmax=10000,tolerance=self._tol,ncv=self._ncv,which='LR')
+        l=np.reshape(vl,(self._D,self._D))
+        r=np.reshape(vr,(self._D,self._D))
+        l=l/np.trace(l)
+        r=r/np.trace(r)
+        
+        self._l=(l+herm(l))/2.0
+        self._r=(r+herm(r))/2.0            
+
+        leftn=np.linalg.norm(np.tensordot(self._A,np.conj(self._A),([0,2],[0,2]))-np.eye(self._D))
+        rightn=np.linalg.norm(np.tensordot(self._B,np.conj(self._B),([1,2],[1,2]))-np.eye(self._D))
+
+        self._lb=mf.initializeLayer(self._A,np.eye(self._D),self._A,self._mpo[0],1) 
+        ihl=mf.addLayer(self._lb,self._A,self._mpo[2],self._A,1)[:,:,0]
+        Elocleft=np.tensordot(ihl,self._r,([0,1],[0,1]))
+        self._rb=mf.initializeLayer(self._B,np.eye(self._D),self._B,self._mpo[2],-1)
+        ihr=mf.addLayer(self._rb,self._B,self._mpo[0],self._B,-1)[:,:,-1]
+        Elocright=np.tensordot(ihr,self._l,([0,1],[0,1]))
+
+        ihlprojected=(ihl-np.tensordot(ihl,l,([0,1],[0,1]))*np.eye(self._D))
+        ihrprojected=(ihr-np.tensordot(r,ihr,([0,1],[0,1]))*np.eye(self._D))
+        
+        self._kleft=mf.RENORMBLOCKHAMGMRES(self._A,self._A,self._l,np.eye(self._D),ihlprojected,x0=np.reshape(self._kleft,self._D*self._D),tolerance=self._lgmrestol,\
+                                           maxiteration=self._Nmaxlgmres,direction=1)
+        self._kright=mf.RENORMBLOCKHAMGMRES(self._B,self._B,np.eye(self._D),self._r,ihrprojected,x0=np.reshape(self._kright,self._D*self._D),tolerance=self._lgmrestol,\
+                                            maxiteration=self._Nmaxlgmres,direction=-1)
+        
+        self._lb[:,:,0]+=np.copy(self._kleft)
+        self._rb[:,:,-1]+=np.copy(self._kright)
+
+        e1,opt=mf.eigsh(self._lb,self._mpo[1],self._rb,self._mps,self._artol,self._arnumvecs,self._arncv)#mps._mat set to 11 during call of __tensor__()
+        e2,self._mat=mf.eigshbond(self._lb,self._mpo[1],self._A,self._rb,self._mat,position='right',tolerance=self._artol,numvecs=self._arnumvecs,numcv=self._arncv)
+        self._mat/=np.linalg.norm(self._mat)
+
+        D1,D2,d=opt.shape
+        
+        if self._svd:
+            ACC_l=np.reshape(ncon.ncon([opt,herm(self._mat)],[[-1,1,-2],[1,-3]]),(D1*d,D2))
+            CAC_r=np.reshape(ncon.ncon([herm(self._mat),opt],[[-1,1],[1,-2,-3]]),(D1,d*D2))
+            Ul,Sl,Vl=mf.svd(ACC_l)
+            Ur,Sr,Vr=mf.svd(CAC_r)
+            self._A=np.transpose(np.reshape(Ul.dot(Vl),(D1,d,D2)),(0,2,1))
+            self._B=np.reshape(Ur.dot(Vr),(D1,D2,d))
+        else:
+            AC_l=np.reshape(np.transpose(opt,(0,2,1)),(D1*d,D2))
+            AC_r=np.reshape(opt,(D1,d*D2))
+            
+            UAC_l,PAC_l=sp.linalg.polar(AC_l,side='right')
+            UAC_r,PAC_r=sp.linalg.polar(AC_r,side='left')
+            
+            UC_l,PC_l=sp.linalg.polar(self._mat,side='right')
+            UC_r,PC_r=sp.linalg.polar(self._mat,side='left')
+            
+            self._A=np.transpose(np.reshape(UAC_l.dot(herm(UC_l)),(D1,d,D2)),(0,2,1))
+            self._B=np.reshape(herm(UC_r).dot(UAC_r),(D1,D2,d))
+
+        return Elocleft,leftn,rightn,
+
+    #important note: at the moment, the user has to provide an mpo which covers TWO unitcells!
+    def __simulate__(self,checkpoint=100):
+        converged=False
+        Dchanged=False
+
+        self._A,self._l=mf.regauge(self._mps,gauge='left',initial=None,nmaxit=10000,tol=self._tol,ncv=self._ncv,numeig=self._numeig)
+        self._B,self._r=mf.regauge(self._mps,gauge='right',initial=None,nmaxit=10000,tol=self._tol,ncv=self._ncv,numeig=self._numeig)
+        self._mat=np.eye(self._A.shape[1])
+        Edensold=1E10
+        while converged==False:
+            Edens,leftn,rightn=self.__doStep__()
+            if self._it>=self._Nmax:
+                break
+            if self._it%checkpoint==0:
+                np.save('CPTensor'+self._filename,self._mps)
+            self._it+=1
+            stdout.write("\rit %i: local E=%.16f, lnorm=%.6f, rnorm=%.6f, D=%i, eps=%.16f" %(self._it,np.real(Edens),leftn,rightn,self._D,np.abs(Edensold-Edens)))
+            stdout.flush()
+            if np.abs(Edensold-Edens)<self._epsilon:
+                converged=True
+            Edensold=Edens
+        print
+        print()
+        if self._it>=self._Nmax and (converged==False):
+            print ('simulation reached maximum number of steps ({1}) and stopped at precision of {0}'.format(np.abs(Edensold-Edens),self._Nmax))
+        if converged==True:
+            print ('simulation converged to {0} in {1} steps'.format(self._epsilon,self._Nmax))
+        print
+ 
+        
 
 class TEBDEngine:
     """

lib/mpslib/mpsfunctions.py
@@ -2224,11 +2224,11 @@ def regauge(tensor,gauge,initial=None,nmaxit=100000,tol=1E-10,ncv=50,numeig=6,pi
         l=np.reshape(v,(chi,chi))
         #fix phase of l and restore the proper normalization of l
         l=l/np.trace(l)
-        if dtype==float:
-            l=np.real(l+herm(l))/2.0
-            
-        if dtype==complex:
-            l=(l+herm(l))/2.0
+        l=(l+herm(l))/2.0        
+        #if dtype==float:
+        #    l=np.real(l+herm(l))/2.0
+        #if dtype==complex:
+        #    l=(l+herm(l))/2.0
 
 
         eigvals,u=np.linalg.eigh(l)
@@ -2263,10 +2263,11 @@ def regauge(tensor,gauge,initial=None,nmaxit=100000,tol=1E-10,ncv=50,numeig=6,pi
         tensor=tensor/np.sqrt(np.real(eta))
         r=np.reshape(v,(chi,chi))
         r=r/np.trace(r)
-        if dtype==float:
-            r=np.real((r+herm(r))/2.0)
-        if dtype==complex:
-            r=(r+herm(r))/2.0
+        r=(r+herm(r))/2.0
+        #if dtype==float:
+        #    r=np.real((r+herm(r))/2.0)
+        #if dtype==complex:
+        #    r=(r+herm(r))/2.0
 
         eigvals,u=np.linalg.eigh(r)
         eigvals=np.abs(eigvals)
@@ -2290,7 +2291,6 @@ def regauge(tensor,gauge,initial=None,nmaxit=100000,tol=1E-10,ncv=50,numeig=6,pi
 
     if gauge=="symmetric":
         [chi1 ,chi2,d]=np.shape(tensor)
-
         [eta,v,numeig]=TMeigs(tensor,direction=1,numeig=numeig,init=initial,nmax=nmaxit,tolerance=tol,ncv=ncv)
 
         if np.abs(np.imag(eta))/np.abs(np.real(eta))>thresh:
@@ -2300,17 +2300,17 @@ def regauge(tensor,gauge,initial=None,nmaxit=100000,tol=1E-10,ncv=50,numeig=6,pi
         l=np.reshape(v,(chi1,chi1))
 
         l=l/np.trace(l)
-        if dtype==float:
-            l=np.real(l+herm(l))/2.0
-            
-        if dtype==complex:
-            l=(l+herm(l))/2.0
-
+        l=(l+herm(l))/2.0
+        #if dtype==float:
+        #    l=np.real(l+herm(l))/2.0
+        #    
+        #if dtype==complex:
+        #    l=(l+herm(l))/2.0
 
         eigvals,u=np.linalg.eigh(l)
         eigvals=np.abs(eigvals)
         eigvals/=np.sum(eigvals)
-        eigvals[np.nonzero(eigvals<pinv)]=0.0
+        eigvals[np.nonzero(eigvals<=pinv)]=0.0
         eigvals/=np.sum(eigvals)
         l=u.dot(np.diag(eigvals)).dot(herm(u))
 
@@ -2328,10 +2328,11 @@ def regauge(tensor,gauge,initial=None,nmaxit=100000,tol=1E-10,ncv=50,numeig=6,pi
             print ('in regauge: warning: found eigenvalue eta with large imaginary part: {0}'.format(eta))
         r=np.reshape(v,(chi2,chi2))
         r=r/np.trace(r)
-        if dtype==float:
-            r=np.real((r+herm(r))/2.0)
-        if dtype==complex:
-            r=(r+herm(r))/2.0
+        r=(r+herm(r))/2.0        
+        #if dtype==float:
+        #    r=np.real((r+herm(r))/2.0)
+        #if dtype==complex:
+        #    r=(r+herm(r))/2.0
 
         eigvals,u=np.linalg.eigh(r)
         eigvals=np.abs(eigvals)
@@ -2345,11 +2346,9 @@ def regauge(tensor,gauge,initial=None,nmaxit=100000,tol=1E-10,ncv=50,numeig=6,pi
         inveigvals[np.nonzero(eigvals>pinv)]=1.0/eigvals[np.nonzero(eigvals>pinv)]
         inveigvals[np.nonzero(eigvals<=pinv)]=0.0
 
-
         r=u.dot(np.diag(eigvals)).dot(herm(u))
         x=u.dot(np.diag(np.sqrt(eigvals))).dot(herm(u))
         invx=u.dot(np.diag(np.sqrt(inveigvals))).dot(herm(u))
-            
         
         [U,lam,Vdag]=svd(y.dot(x))
         D=len(lam)
@@ -2369,7 +2368,7 @@ def regauge(tensor,gauge,initial=None,nmaxit=100000,tol=1E-10,ncv=50,numeig=6,pi
             Vdag=Vdag[0:len(lam),:]
             Z=np.linalg.norm(lam)
             lam=lam/Z
-
+            
         gamma=np.tensordot(Vdag.dot(invx),tensor,([1],[0]))
         gamma=np.transpose(np.tensordot(gamma,invy.dot(U),([1],[0])),(0,2,1))
         A=np.tensordot(np.diag(lam),gamma,([1],[0]))