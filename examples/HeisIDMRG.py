66e7b6391d4d38f097ed74a3ca47efe0dea4a846
Martin <martin.ganahl@gmail.com>
2019-03-08 11:33:56 -0500

    parameter change

--- examples/HeisIDMRG.py
+++ examples/HeisIDMRG.py
@@ -21,7 +21,7 @@ herm=lambda x:np.conj(np.transpose(x))
 if __name__ == "__main__":
     D=50
     d=2
-    N=2
+    N=6
     Jz=np.ones(N)
     Jxy=np.ones(N)
     #initialize MPS with bond dimension D
@@ -33,6 +33,6 @@ if __name__ == "__main__":
     #idmrg.__simulateTwoSite__(Nmax=1000,NUC=1,Econv=1E-10,tol=1E-4,ncv=10,cp=None,verbose=1,truncation=1E-8,regaugestep=3) #two site idmrg
     print(idmrg.__doc__)
     print(idmrg.simulate.__doc__)
-    idmrg.simulate(Nmax=100,NUC=2,solver='AR',Econv=1E-10,tol=1E-6,ncv=20,cp=None,verbose=1,regaugestep=0) #single site idmrg
+    idmrg.simulate(Nmax=100,NUC=4,solver='AR',Econv=1E-10,tol=1E-6,ncv=20,cp=None,verbose=1,regaugestep=0) #single site idmrg
 
     
