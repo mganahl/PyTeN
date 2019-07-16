1e1c697f1e911cf660ee6ac897e9e3e09ce3b557
Martin <martin.ganahl@gmail.com>
2019-07-16 12:43:13 -0400

    added overlap minimizer

--- lib/mpslib/SimContainer.py
+++ lib/mpslib/SimContainer.py
@@ -21,6 +21,7 @@ import lib.ncon as ncon
 from lib.mpslib.Container import Container
 from lib.mpslib.TensorNetwork import FiniteMPS, MPS
 from scipy.sparse.linalg import ArpackNoConvergence
+from lib.mpslib.Tensor import Tensor
 import time
 comm = lambda x, y: np.dot(x, y) - np.dot(y, x)
 anticomm = lambda x, y: np.dot(x, y) + np.dot(y, x)
@@ -1924,10 +1925,179 @@ class FiniteTDVPEngine(TDVPEngine):
         super().__init__(mps, mpo, lb, rb, name=name)
 
 
-
-
-
         
+class OverlapMinimizer(Container):
+    def __init__(self, mps, conj_mps, gates, name='overlap_minimizer'):
+        super().__init__(name=name)
+        self.mps = mps
+        self.conj_mps = conj_mps
+        self.gates = gates
+        self.right_envs = {}
+        self.left_envs = {}        
+
+    #def minimize_overlap(self, num_steps):
+
+    @staticmethod
+    def add_unitary_right(site, right_envs, mps, conj_mps, gates):
+        #if site is even, add an odd gate
+        #if site is odd, add an even gate
+        assert(site>0)
+        assert(len(mps)%2==0)
+        if site == (len(mps) - 1):
+            right_envs[site - 1] = ncon.ncon([mps.get_tensor(site), 
+                                            np.conj(conj_mps.get_tensor(site)), gates[(site-1,site)]],
+                                           [[-1,1,2],[-2,1,3],[-4,3,-3,2]])        
+        elif (site < len(mps) - 1) and (site % 2 == 0):
+            right_envs[site - 1] = ncon.ncon([right_envs[site], mps.get_tensor(site), 
+                                            np.conj(conj_mps.get_tensor(site)), gates[(site-1,site)]],
+                                           [[1,3,2,5],[-1,1,2],[-2,3,4],[-4,4,-3,5]])
+        elif (site < len(mps) - 1) and (site % 2 == 1):
+            right_envs[site - 1] = ncon.ncon([right_envs[site], mps.get_tensor(site), 
+                                            np.conj(conj_mps.get_tensor(site)), gates[(site-1,site)]],
+                                           [[1,4,3,5],[-1,1,2],[-2,4,5],[-4,3,-3,2]])
+    @staticmethod            
+    def add_unitary_left(site, left_envs, mps, conj_mps, gates):
+        #if site is even, add an odd gate
+        #if site is odd, add an even gate
+        assert(site<len(mps))
+        assert(len(mps)%2==0)
+            
+        if site == 0:
+            left_envs[site + 1] = ncon.ncon([mps.get_tensor(site), 
+                                             np.conj(conj_mps.get_tensor(site)), gates[(site, site + 1)]],
+                                            [[1,-1,2],[1, -2, 3],[3, -4, 2, -3]])        
+        elif (site > 0) and (site % 2 == 0):
+            left_envs[site + 1] = ncon.ncon([left_envs[site], mps.get_tensor(site), 
+                                              np.conj(conj_mps.get_tensor(site)), gates[(site, site + 1)]],
+                                             [[1,3,5,4],[1,-1,2],[3,-2,4],[5,-4,2,-3]])
+        elif (site > 0) and (site % 2 == 1):
+            left_envs[site + 1] = ncon.ncon([left_envs[site], mps.get_tensor(site), 
+                                             np.conj(conj_mps.get_tensor(site)), gates[(site, site + 1)]],
+                                             [[1,3,2,5],[1,-1,2],[3,-2,4],[4,-4,5,-3]])  
+    @staticmethod            
+    def get_env(sites,left_envs,right_envs, mps, conj_mps):
+        assert((len(mps) % 2) == 0)
+        if (sites[0] > 0) and (sites[1] < len(mps) - 1) and (sites[0] % 2 == 0):
+            return ncon.ncon([left_envs[sites[0]], mps.get_tensor(sites[0]), 
+                       mps.get_tensor(sites[1]), right_envs[sites[1]], 
+                       conj_mps.get_tensor(sites[0]).conj(), conj_mps.get_tensor(sites[1]).conj()],
+                      [[1, 2, -1, 3], [1, 4, -3], [4, 8, -4], [8, 7, -2, 6], [2, 5, 3], [5, 7, 6]])
+        elif (sites[0] > 0) and (sites[1] < len(mps) - 1) and (sites[0] % 2 == 1):
+            return ncon.ncon([left_envs[sites[0]], mps.get_tensor(sites[0]), 
+                       mps.get_tensor(sites[1]), right_envs[sites[1]], 
+                       conj_mps.get_tensor(sites[0]).conj(), conj_mps.get_tensor(sites[1]).conj()],
+                      [[1, 7, 2, -3], [1, 3,  2], [3, 5, 4], [5, 8, 4, -4], [7, 6, -1], [6, 8, -2]])    
+        elif sites[0] == 0:
+            return ncon.ncon([mps.get_tensor(sites[0]), 
+                               mps.get_tensor(sites[1]), right_envs[sites[1]], 
+                               conj_mps.get_tensor(sites[0]).conj(), conj_mps.get_tensor(sites[1]).conj()],
+                              [[1,2,-3], [2,3,-4], [3,4,-2,5], [1,6,-1], [6,4,5]])  
+        elif sites[1] == (len(mps) - 1):
+            return ncon.ncon([left_envs[sites[0]], mps.get_tensor(sites[0]), 
+                               mps.get_tensor(sites[1]),
+                               conj_mps.get_tensor(sites[0]).conj(), conj_mps.get_tensor(sites[1]).conj()],
+                              [[5,4, -1,3], [5,1,-3], [1,6,-4], [4,2,3], [2,6,-2]])
+    @staticmethod        
+    def overlap(site,left_envs,right_envs, mps, conj_mps):
+        if site%2 == 1:
+            return ncon.ncon([left_envs[site], mps.get_tensor(site), conj_mps.get_tensor(site).conj(),
+                              right_envs[site]],
+                             [[1,5,2,4], [1,3,2], [5,7,6], [3,7,4,6]])
+        elif site%2 == 0:
+            return ncon.ncon([left_envs[site], mps.get_tensor(site), conj_mps.get_tensor(site).conj(),
+                              right_envs[site]],
+                             [[1,5,4,6], [1,3,2], [5,7,6], [3,7,2,4]])
+    @staticmethod
+    def u_update_svd_numpy(wIn):
+        """
+        obtain the update to the disentangler using numpy svd
+        Fixme: this currently only works with numpy arrays
+        Args:
+            wIn (np.ndarray or Tensor):  unitary tensor of rank 4
+        Returns:
+            The svd update of `wIn`
+        """
+        shape = np.shape(wIn)
+        ut, st, vt = np.linalg.svd(
+            np.reshape(wIn, (shape[0] * shape[1], shape[2] * shape[3])),
+            full_matrices=False)
+        return -np.reshape(ncon.ncon([ut, vt], [[-1, 1], [1, -2]]), shape).view(Tensor)
+        
+
+    def minimize(self,num_iterations, verbose=0):
+        [self.add_unitary_right(site, self.right_envs, self.mps, self.conj_mps, self.gates) for site in reversed(range(1,len(self.mps)))]        
+        for it in range(num_iterations):
+            for site in range(len(self.mps) - 1):
+                env = self.get_env((site,site+1), self.left_envs, self.right_envs, self.mps, self.conj_mps)
+                self.gates[(site, site+1)] = self.u_update_svd_numpy(env)
+                self.add_unitary_left(site,self.left_envs, self.mps, self.conj_mps, self.gates)
+                if verbose > 0 and site > 0:
+                    overlap = self.overlap(site, self.left_envs, self.right_envs, self.mps, self.conj_mps)                    
+                    stdout.write(
+                        "\r iteration  %i/%i, overlap = %.16f" %
+                        (it, num_iterations, np.abs(np.real(overlap))))
+                    stdout.flush()
+                if verbose > 1:
+                    print()
+
+            for site in reversed(range(1, len(self.mps) - 1)):
+                env = self.get_env((site,site+1), self.left_envs, self.right_envs, self.mps, self.conj_mps)
+                self.gates[(site, site+1)] = self.u_update_svd_numpy(env)
+                self.add_unitary_right(site + 1, self.right_envs, self.mps, self.conj_mps, self.gates)
+                if verbose > 0 and site > 0:
+                    overlap = self.overlap(site, self.left_envs, self.right_envs, self.mps, self.conj_mps)
+                    stdout.write(
+                        "\r iteration  %i/%i, overlap = %.16f" %
+                        (it, num_iterations, np.abs(np.real(overlap))))
+                    stdout.flush()
+                if verbose > 1:
+                    print()
+
+    def minimize_even(self,num_iterations, verbose=0):
+        self.left_envs = {}
+        self.right_envs = {}        
+
+        for it in range(num_iterations):
+            [self.add_unitary_right(site, self.right_envs, self.mps, self.conj_mps, self.gates) for site in reversed(range(1,len(self.mps)))]                    
+            for site in range(0,len(self.mps) - 1, 2):
+                env = self.get_env((site, site + 1), self.left_envs, self.right_envs, self.mps, self.conj_mps)
+                self.gates[(site, site+1)] = self.u_update_svd_numpy(env)
+                self.add_unitary_left(site, self.left_envs, self.mps, self.conj_mps, self.gates)
+                if site + 1 < len(self.mps) - 1:
+                    self.add_unitary_left(site + 1,self.left_envs, self.mps, self.conj_mps, self.gates)                
+                if verbose > 0 and site > 0:
+                    overlap = self.overlap(site, self.left_envs, self.right_envs, self.mps, self.conj_mps)                    
+                    stdout.write(
+                        "\r iteration  %i/%i at site %i , overlap = %.16f" %
+                        (it, num_iterations, site, np.abs(np.real(overlap))))                        
+                    stdout.flush()
+                if verbose > 1:
+                    print()
+
+
+    def minimize_odd(self,num_iterations, verbose=0):
+        self.left_envs = {}
+        self.right_envs = {}        
+        for it in range(num_iterations):
+            [self.add_unitary_right(site, self.right_envs, self.mps, self.conj_mps, self.gates) for site in reversed(range(1,len(self.mps)))]
+            self.add_unitary_left(0, self.left_envs, self.mps, self.conj_mps, self.gates)
+            
+            for site in range(1,len(self.mps) - 2, 2):
+                env = self.get_env((site, site + 1), self.left_envs, self.right_envs, self.mps, self.conj_mps)
+                self.gates[(site, site+1)] = self.u_update_svd_numpy(env)
+                self.add_unitary_left(site, self.left_envs, self.mps, self.conj_mps, self.gates)
+                if site + 1 < len(self.mps) - 1:
+                    self.add_unitary_left(site + 1,self.left_envs, self.mps, self.conj_mps, self.gates)                
+                if verbose > 0 and site > 0:
+                    overlap = self.overlap(site, self.left_envs, self.right_envs, self.mps, self.conj_mps)
+                    stdout.write(
+                        "\r iteration  %i/%i at site %i , overlap = %.16f" %
+                        (it, num_iterations, site, np.abs(np.real(overlap))))                        
+                    stdout.flush()
+                if verbose > 1:
+                    print()
+
+                    
 # class VUMPSengine(InfiniteDMRGEngine):
 #     """
 #     VUMPSengine
